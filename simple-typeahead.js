204907d3484afa663d4be50929ae37421e9cfb7a
Ravikiran <ravikirankb@gmail.com>
2019-11-12 05:00:49 +0000

    Initial commit
    Added basic jquery function on input element

--- simple-typeahead.js
+++ simple-typeahead.js
@@ -1,1 +1,39 @@
 
+(function ($) {
+    $.fn.simple_typeahead = function (options) {
+
+
+        function Typeahead(el, args) {
+            let plugin = this;
+            let element = el;
+
+            var settings = $.extend({
+                data: null,
+                onSelected: function () {
+
+                },
+                displayExpr: 'Name',
+                keyExpr: 'Id',
+                minSuggestions: 5,
+                autoCompleteEnabled: false,
+                placeHolderText: '--Select--',
+                nosuggestionsText: '--No Suggestions--',
+                minChars: 1,
+                isremoteoptionenabled: false,
+                orientation: 'bottom',
+                highlightsearchkey: false
+            }, args);
+
+            plugin.element = el;
+            plugin.settings = settings;
+            
+
+        }
+
+        return this.each(function () {
+            var element = $(this);
+
+            var instance = this.Typeahead(element, options);
+        })
+    }
+})(jQuery)
